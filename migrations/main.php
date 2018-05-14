641237c81bd026ed3e9b89be1c1d67d02e62ffa6
Suzunone <suzunone.eleven@gmail.com>
2018-05-15 01:53:23 +0900

    migrator mod

---
+++ migrations/main.php
@@ -0,0 +1,17 @@
+<?php
+$autoloadFiles = [
+    __DIR__ . '/vendor/autoload.php',
+];
+$autoloader = false;
+foreach ($autoloadFiles as $autoloadFile) {
+    if (file_exists($autoloadFile)) {
+        require_once $autoloadFile;
+        $autoloader = true;
+    }
+}
+if (!$autoloader) {
+    if (extension_loaded('phar') && ($uri = Phar::running())) {
+        echo 'The phar has been built without dependencies' . PHP_EOL;
+    }
+    die('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
+}
